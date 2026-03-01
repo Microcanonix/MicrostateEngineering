f46e68767afc04f76b31b730ddce2a78ad8d4cd9
Microcanonix <piet.demeester@microcanonix.com>
2026-03-01 12:49:45 +0100

    Add needed code to Application

--- MainConsole/Application.cs
+++ MainConsole/Application.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace MainConsole
 {
     public sealed class Application : BackgroundService
     {
+        private readonly ILogger<Application> _logger;
+
+        public Application(ILogger<Application> logger)
+        {
+            _logger = logger;
+        }
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            await Task.CompletedTask;
+            _logger.LogInformation("Application started");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Application running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(2000, stoppingToken);
+            }
+
+            _logger.LogInformation("Application stopping");
 
         }
     }
