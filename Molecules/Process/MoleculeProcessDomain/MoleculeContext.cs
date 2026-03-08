1deb313264603fde1bf42e799e0ae9c8f8b37dca
Microcanonix <piet.demeester@microcanonix.com>
2026-03-08 17:04:05 +0100

    Add context

--- Molecules/Process/MoleculeProcessDomain/MoleculeContext.cs
+++ Molecules/Process/MoleculeProcessDomain/MoleculeContext.cs
@@ -2,6 +2,16 @@
 {
     public sealed class MoleculeContext
     {
+        public required string Basisset             { get; set; }
+        public required string MoleculeName         { get; set; }
+
+
+        public required string PackageRoot                  { get; set; }
+        public required string XyzfilesFolder             { get; set; }
+        public required string GmsInputFolder             { get; set; }
+        public required string GmsOutputFolder            { get; set; }
+        public required string WorkflowStatusFolder { get; set; }
+        public required string MoleculeDataFolder   { get; set; }
 
     }
 }
