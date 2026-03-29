9ba74503ae5657408f4c14030134460b80d6272d
Microcanonix <piet.demeester@microcanonix.com>
2026-03-29 20:21:46 +0200

    Implement Handle Imprt Data

--- Molecules/Process/MoleculeProcessService/MoleculeProcessService.cs
+++ Molecules/Process/MoleculeProcessService/MoleculeProcessService.cs
@@ -2,6 +2,7 @@
 using IMoleculeProcessServices;
 using IMoleculeServices;
 using Microsoft.Extensions.Logging;
+using MoleculeDomain;
 using MoleculeProcessDomain;
 
 namespace MoleculeProcessService
@@ -25,11 +26,50 @@ namespace MoleculeProcessService
 
         public async Task<MoleculeGmsResult> HandleImportData(MoleculeContext context)
         {
-            return await Task.FromResult(new MoleculeGmsResult()
+            try
             {
-                IsSuccess = false,
-                Message = "NotImplemented"
-            });
+                if (! context.CanExecute )
+                {
+                    return new MoleculeGmsResult()
+                    {
+                        IsSuccess = true,
+                        Message = $"Molecule {context.MoleculeName} was skipped"
+                    };
+                }
+
+                MoleculeGmsResult result;
+                var xyzDirectory = Path.Combine(context.PackageRoot, context.XyzfilesFolder);
+                var moleculeDirectory = Path.Combine(context.PackageRoot, context.MoleculeDataFolder);
+                var existingMolecule = _moleculeService.GetMolecule(moleculeDirectory, context.MoleculeName);
+                if (existingMolecule is null )
+                {
+                    var moleculeFromXyzFile = await _moleculeService.InitMoleculeFromXyzFileAsync(xyzDirectory, context.MoleculeName, context.Charge);
+                    await _moleculeService.SaveMoleculesAsync(new List<Molecule>() { moleculeFromXyzFile }, moleculeDirectory); 
+                    result = new MoleculeGmsResult()
+                    {
+                        IsSuccess = true,
+                        Message = $"Molecule {moleculeFromXyzFile.Name} was initialised"
+                    };
+                }
+                else
+                {
+                    result = new MoleculeGmsResult()
+                    {
+                        IsSuccess = true,
+                        Message = $"Molecule {existingMolecule.Name} allready existed"
+                    };
+                }
+                return result;
+            }
+            catch(Exception exc)
+            {
+                _logger.LogCritical(exc, "An error occured while Importing Data");
+                return new MoleculeGmsResult()
+                {
+                    IsSuccess = false,
+                    Message = $"An unexpected error happened {exc.Message}"
+                };
+            }
         }
 
         public async Task<MoleculeGmsResult> HandleGeometryOptimization(MoleculeContext context)
