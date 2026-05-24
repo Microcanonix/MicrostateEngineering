329dcd0c898c1f75512f774b7606d7a19f0ce031
Microcanonix <piet.demeester@microcanonix.com>
2026-05-24 16:43:39 +0200

    remove avalonia project

--- MoleculesViewer/App.xaml.cs
+++ MoleculesViewer/App.xaml.cs
@@ -1,6 +1,17 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
+using MoleculeFactory;
+using MoleculeProcessFactory;
+using MoleculeProcessService;
+using MoleculeRepository;
+using MoleculeServices;
+using ResearchDefinitionRepository;
+using ResearchDefinitionService;
+using System.Configuration;
 using System.Data;
+using System.Diagnostics.CodeAnalysis;
 using System.Windows;
+using System.Windows.Threading;
+using UtilityServices;
 
 namespace MoleculesViewer
 {
@@ -9,6 +20,37 @@ namespace MoleculesViewer
     /// </summary>
     public partial class App : Application
     {
+        public IServiceProvider? Services { get; set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var serviceCollection = new ServiceCollection();
+
+            Register(serviceCollection, ServiceLifetime.Singleton);
+
+            Services = serviceCollection.BuildServiceProvider();
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+        }
+
+        public IServiceCollection Register(IServiceCollection services, ServiceLifetime serviceLifetime)
+        {
+            return services.RegisterUtilities(serviceLifetime)
+                            .RegisterResearchDefinitionRepo(serviceLifetime)
+                            .RegisterResearchDefinitionSvc(serviceLifetime)
+                            .RegisterMoleculeRepository(serviceLifetime)
+                            .RegisterMoleculeFactory(serviceLifetime)
+                            .RegisterMoleculeService(serviceLifetime)
+                            .RegisterMoleculeProcessService(serviceLifetime)
+                            .RegisterMoleculeProcessFactory(serviceLifetime);
+
+        }
+
     }
 
 }
