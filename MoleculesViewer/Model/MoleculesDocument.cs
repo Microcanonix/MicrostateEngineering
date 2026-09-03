d0e829498e54d45f98fd046d91e5cf21f60ccef6
Microcanonix <piet.demeester@microcanonix.com>
2026-09-03 22:13:06 +0200

    update

--- MoleculesViewer/Model/MoleculesDocument.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MoleculesViewer.Model
-{
-    public sealed class MoleculesDocument
-    {
-        public required string SourcePath { get; set; }
-    }
-}
--- MoleculesViewer/ViewModels/WorkflowExecuterControlViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using MoleculesViewer.Common;
-
-namespace MoleculesViewer.ViewModels
-{
-    public sealed class WorkflowExecuterControlViewModel: ObservableObject
-    {
-        public string Title { get; } = "Hello I am a workflow Executor";
-    }
-}
