65656691560598f08e59c06fae06d310e6bed121
Microcanonix <piet.demeester@microcanonix.com>
2026-09-05 11:16:28 +0200

    Add Angular to apphost

--- MicrostateEngineering.AppHost/AppHost.cs
+++ MicrostateEngineering.AppHost/AppHost.cs
@@ -1,6 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.MainConsole>("mainconsole");
+builder.AddProject<Projects.MainConsole>("mainconsole")
+    .WithExplicitStart();
 
+builder.AddViteApp(
+        "microstate-web",
+        "../Web",
+        runScriptName: "start")
+    .WithExternalHttpEndpoints()
+    .WithExplicitStart();
 
 builder.Build().Run();
