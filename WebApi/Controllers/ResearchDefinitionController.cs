0ef7d3226f00bd56df25d4e9854c3fbc3dc5a9f2
Microcanonix <piet.demeester@microcanonix.com>
2026-09-20 17:25:31 +0200

    Expose YAML research definition endpoints

--- WebApi/Controllers/ResearchDefinitionController.cs
+++ WebApi/Controllers/ResearchDefinitionController.cs
@@ -1,4 +1,5 @@
 using IResearchDefintionService;
+using IUtilitiesServices;
 using Microsoft.AspNetCore.Mvc;
 using ResearchDefinitionDomain.GamessCalculation;
 
@@ -9,16 +10,36 @@ namespace WebApi.Controllers
     public class ResearchDefinitionController : ControllerBase
     {
         private readonly IResearchDefinitionService _researchDefinitionService;
+        private readonly IYamlParser<MoleculesResearchDefinition> _yamlParser;
 
-        public ResearchDefinitionController(IResearchDefinitionService researchDefinitionService)
+        public ResearchDefinitionController(
+            IResearchDefinitionService researchDefinitionService,
+            IYamlParser<MoleculesResearchDefinition> yamlParser)
         {
             _researchDefinitionService = researchDefinitionService;
+            _yamlParser = yamlParser;
         }
 
         [HttpGet(Name = "ResearchDefinition")]
         public IEnumerable<MoleculesResearchDefinition> Get()
         {
             return _researchDefinitionService.GetMoleculesResearchDefinitions();
+        }
+
+        [HttpGet("{researchDefinitionName}/yaml")]
+        public IActionResult GetYaml(string researchDefinitionName)
+        {
+            var researchDefinition = _researchDefinitionService
+                .GetMoleculesResearchDefinitions()
+                .FirstOrDefault(definition =>
+                    string.Equals(definition.Name, researchDefinitionName, StringComparison.OrdinalIgnoreCase));
+
+            if (researchDefinition == null)
+            {
+                return NotFound();
+            }
+
+            return Content(_yamlParser.Serialize(researchDefinition), "text/yaml");
         }
 
         [HttpPost]
@@ -29,6 +50,19 @@ namespace WebApi.Controllers
                 return BadRequest();
             }
 
+            _researchDefinitionService.SaveMoleculesResearchDefintion(researchDefinition);
+            return Ok();
+        }
+
+        [HttpPost("yaml")]
+        public IActionResult SaveYaml([FromBody] SaveResearchDefinitionYamlRequest request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Yaml))
+            {
+                return BadRequest();
+            }
+
+            var researchDefinition = _yamlParser.Parse(request.Yaml);
             _researchDefinitionService.SaveMoleculesResearchDefintion(researchDefinition);
             return Ok();
         }
@@ -45,4 +79,6 @@ namespace WebApi.Controllers
             return NoContent();
         }
     }
+
+    public sealed record SaveResearchDefinitionYamlRequest(string Yaml);
 }
