a2cacffacfd8ef10d52601c20255a64eff416989
Microcanonix <piet.demeester@microcanonix.com>
2026-09-20 17:25:36 +0200

    Model API-backed workflows

--- Web/src/app/features/workflows/components/workflow.model.ts
+++ Web/src/app/features/workflows/components/workflow.model.ts
@@ -1,3 +1,20 @@
+export interface ResearchDefinitionProcess {
+  type: number;
+}
+
+export interface ResearchDefinition {
+  name: string;
+  packageRoot: string;
+  xyzfiles: string;
+  gmsInput: string;
+  gmsOutput: string;
+  workflowStatusFolder: string;
+  moleculeData: string;
+  basisset: string;
+  molecules: unknown[];
+  processes: ResearchDefinitionProcess[];
+}
+
 export interface WorkflowSummary {
   id: string;
   name: string;
