cc3f8296e86510ed1c64a69ac2fd146bba0f8c0e
Microcanonix <piet.demeester@microcanonix.com>
2026-09-20 17:25:33 +0200

    Provide Angular HttpClient

--- Web/src/app/app.config.ts
+++ Web/src/app/app.config.ts
@@ -3,6 +3,7 @@ import {
   provideBrowserGlobalErrorListeners,
   importProvidersFrom,
 } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -10,6 +11,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideHttpClient(),
     provideRouter(routes),
     importProvidersFrom(NgbModule),
   ],
